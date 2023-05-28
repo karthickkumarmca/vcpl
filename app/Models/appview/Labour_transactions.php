4bd9fd0fc343d4bbaaf33c0a54021bcd1e679416
yuvaraj2612 <yuvaraj2612@gmail.com>
2023-05-28 21:20:37 +0530

    LorrymovementFixesUpdates
    LorrymovementFixesUpdates

--- app/Models/appview/Labour_transactions.php
+++ app/Models/appview/Labour_transactions.php
@@ -15,9 +15,9 @@ class Labour_transactions extends Model
     protected $table = 'labour_movement_list';
     public static function storeRecords($request)
     {
-        $site_id = Session::get('site_id');
-
+        $site_id = Session::get('site_id');        
         $numberOfLabour            = !empty($request->number_of_labour)?$request->number_of_labour:0;
+        $labourDate                = !empty($request->labour_date)?$request->labour_date:'';
         if(count($numberOfLabour)>0) {
         	$strUuid = \Str::uuid()->toString();
         	foreach($numberOfLabour as $k=>$l){
@@ -26,9 +26,9 @@ class Labour_transactions extends Model
 		        $data->created_at          = date('Y-m-d H:i:s');
 		        $data->uuid                = $strUuid;
 		        $data->subcontractor_id    = $request->subcontractor_id;
-		        $data->labour_working_date = $request->labour_date;
+		        $data->labour_date         = $labourDate;
                 $data->labour_category_id  = $k;
-		        $data->number_of_labour    = $l;
+		        $data->number_of_labour    = !empty($l)?$l:0;
 		        $data->shift_id            = $request->shift_id;
 		        $data->save();
 	        }
